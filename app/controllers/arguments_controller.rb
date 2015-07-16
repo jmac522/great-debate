74f91dfe05bc9bd23076c613ce716feac0af91c6
jmac522 <johnmccarthy@John-McCarthys-MacBook-Pro-2.local>
2015-07-16 11:07:37 -0400

    added arguments

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -1,11 +1,15 @@
 class ArgumentsController < ApplicationController
 
   def create
-    binding.pry
-    if @debate_participant.save
-      @debate.turn = @debate_participant.side.title
-      @debate.save
-      flash[:notice] = "You joined the debate!"
+    @debate = Debate.find(params[:debate_id])
+    @argument = Argument.new(body: params[:argument][:body], debate: @debate)
+    @debate_participant = DebateParticipant.find_by(user: current_user,
+                                                    debate: @debate)
+    @argument.debate_participant = @debate_participant
+    @argument.side = @debate_participant.side
+    if @argument.save
+      @debate.swap_turn
+      flash[:notice] = "Argument successfully submitted!"
       redirect_to debate_path(@debate)
     else
       flash[:notice] = @debate_participant.errors.full_messages
