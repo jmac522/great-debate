892031005b0007e65023a4ca06cfd22f7b45dfd8
jmac522 <johnmccarthy@John-McCarthys-MacBook-Pro-2.local>
2015-07-24 11:21:30 -0400

    test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,9 @@
 class HomesController < ApplicationController
   def index
-    @top_argument = Argument.all.order("score DESC").first
-    @argument_user = @top_argument.debate_participant.user
-    @recent_debates = Debate.all.order("created_at DESC").limit(3)
-    @top_users = User.all.order("xp DESC").limit(10)
-    @recent_topics = Topic.all.order("created_at DESC").limit(3)
+    # @top_argument = Argument.all.order("score DESC").first
+    # @argument_user = @top_argument.debate_participant.user
+    # @recent_debates = Debate.all.order("created_at DESC").limit(3)
+    # @top_users = User.all.order("xp DESC").limit(10)
+    # @recent_topics = Topic.all.order("created_at DESC").limit(3)
   end
 end
